b141192baa355dc4da4e48dceeb1192cee1bc296
sebaassssssssss <sebascrzesq123@gmail.com>
2024-09-14 22:19:30 -0600

    updated files

---
+++ README.md
@@ -0,0 +1,1 @@
+<em>README de prueba</em>
--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json');
+
+app.get("/", (req, res, next) => {
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
+});
+
+app.get("/pokemon/all", (req, res, next) => {
+    res.status(200)
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const id = req.params.id -1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    } else {
+        res.status(404);
+        res.send("Pokemon no encontrado.")
+    }
+    
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++){
+        if(pokemon[i].name == name){
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado.")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running...")
+});
+
+
+
 
 /*
 Verbos HTTP
@@ -9,11 +51,3 @@ PATCH = actualización de un dato de un recurso (ej. base de datos) específico
 PUT = modifica todos los elementos de un recurso
 DELETE = eliminar un recurso (ej. un registro de una base de datos)
 */
-app.get("/", (req, res, next) => {
-    res.status(200)
-    res.send("Bienvenido");
-});
-
-app.listen(3000, () => {
-    console.log("Server is running...")
-});
