cd43cda8a9fa9a940c5460a7e926a0317b6d5c96
sebaassssssssss <sebascrzesq123@gmail.com>
2024-09-28 23:09:32 -0600

    added pokemon POST

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -2,13 +2,28 @@ const express = require('express');
 const pokemon = express.Router();
 const db = require('../config/database')
 
-pokemon.post("/", (req, res, next) => {
-    return res.status(200).json(req.body);    
+pokemon.post("/", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+
+        let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+        query +=  `VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+        const rows = await db.query(query);
+
+        if(rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "Pokemon insertado correctamente."});
+        }
+        
+        return res.status(500).json({code: 500, message: "Ocurrió un error."})
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos."})
+
 });
     
 pokemon.get("/", async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({code: 1, message: pkmn});
+    return res.status(200).json({code: 200, message: pkmn});
 });
     
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
